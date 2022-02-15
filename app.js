92fa94737c31c5bb39085ad2522001211607dfdd
Gajraj Mohini <mohinigajraj145@gmail.com>
2022-02-15 16:10:29 +0530

    changed port

--- app.js
+++ app.js
@@ -59,4 +59,4 @@ app.post("/failure", (req,res) =>{
     res.redirect("/");
 })
 
-app.listen(3000, () => console.log("Server started on port 3000"));
+app.listen(process.env.PORT || 3000, () => console.log("Server started on port 3000"));
